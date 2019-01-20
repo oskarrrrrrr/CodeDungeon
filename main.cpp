986657a9cd23bfaee881b78b86cd1791824a84a1
oskarrrrrrr <o.tolkacz@gmail.com>
2019-01-20 21:31:54 +0100

    Add example main.c
    Example prints a hardcoded map

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+#include <MapGenerator/IMapGenerator/ITerrainGenerator.h>
+#include <MapGenerator/HardcodeMapGenerator/HardcodeTerrainGenerator.h>
+#include <MapGenerator/Seed.h>
+
+#include <memory>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World!";
-    return 0;
+	std::unique_ptr<ITerrainGenerator> terrain_generator(new HardcodeTerrainGenerator);
+	std::unique_ptr<Seed> s(new Seed);
+	Terrain terrain = terrain_generator.get()->generate(*(s.get()));
+	std::cout << terrain.toString();
+  return 0;
 }
