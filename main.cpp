01201930551619b6efdf0a86f610a880d5c5cb5f
Krzysztof Bednarek <krzybe@gmail.com>
2019-01-23 00:13:31 +0100

    changed main.cpp to work with curses library

--- main.cpp
+++ main.cpp
@@ -2,14 +2,42 @@
 #include <MapGenerator/HardcodeMapGenerator/HardcodeTerrainGenerator.h>
 #include <MapGenerator/Seed.h>
 
+#include <curses.h>
 #include <memory>
 #include <iostream>
 
+void initialize()
+{
+	initscr();
+
+	cbreak();
+	keypad(stdscr, TRUE);
+	intrflush( stdscr, FALSE );
+	noecho();
+	timeout(0); // non-blocking read
+}
+
+void finalize()
+{
+	endwin();
+}
+
 int main()
 {
+	initialize();
+
 	std::unique_ptr<ITerrainGenerator> terrain_generator(new HardcodeTerrainGenerator);
 	std::unique_ptr<Seed> s(new Seed);
 	Terrain terrain = terrain_generator.get()->generate(*(s.get()));
-	std::cout << terrain.toString();
-  return 0;
+
+    waddstr( stdscr, const_cast<char*>(terrain.toString().c_str()) );
+
+    wmove( stdscr, 22, 3 );
+    waddstr( stdscr, "Press any key to exit" );
+    timeout(-1); // make blocking again
+    wgetch(stdscr);
+
+    finalize();
+
+    return 0;
 }
