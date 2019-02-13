29a0d476187fa1d99593051ca64d03bd3eddfa07
Krzysztof Bednarek <krzybe@gmail.com>
2019-02-13 14:09:14 +0100

    Added simple bot player logic

--- Sources/MapGenerator/BotPlayer.cpp
+++ Sources/MapGenerator/BotPlayer.cpp
@@ -2,9 +2,16 @@
 // Created by dawidbarzyk on 2/12/19.
 //
 
+#include <MapGenerator/RandomGenerator.h>
+#include <utilities/seedGenerator.h>
+
+
 #include "BotPlayer.h"
 
 Action BotPlayer::genAction(const Map &map) const
 {
-    return Quit{};
+    uint64_t seed = createSeed();
+    RandomGenerator randGen{seed};
+
+    return Move{static_cast<Direction>(randGen.generate(0, 3))};
 }
